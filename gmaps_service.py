d4426b4b5b9c5b0325409ae9125f6e4b9bbdba7a
samzaberi <samz.aberi@gmail.com>
2019-07-24 19:43:27 +0300

    added google maps api

---
+++ gmaps_service.py
@@ -0,0 +1,36 @@
+
+import googlemaps
+import json
+
+
+def req_dist_matrix(lat, longt, locs):
+    gmaps = googlemaps.Client(key='AIzaSyCSMn6DIKRxyKHzfO-mhjnqU6u6ZHkUbgE')
+
+    ids = [x[0] for x in locs]
+    origins = [(x[1], x[2]) for x in locs]
+
+    distmatrix_result = gmaps.distance_matrix(
+        origins=origins, destinations=(lat, longt))
+
+    req_details = []
+    for item in distmatrix_result['rows']:
+        for x in item['elements']:
+            req_details.append(
+                {'dist_text': x['distance']['text'],
+                 'distance': x['distance']['value'],
+                 'duration': x['duration']['text']})
+    results = []
+    for i, a in zip(ids, req_details):
+        results.append({'id': i, 'details': a})
+
+    return results
+
+
+locdata = [
+    (1, -1.0984512, 37.0107431),
+    (2, -1.0994236450388715, 37.0120918750763),
+    (3, -1.098136421449109, 37.016737461090095),
+    (4, -1.1003032475060555, 37.019698619842536)
+]
+
+print(req_dist_matrix(1.0985132000000002, 37.0108526, locdata))
--- lance.py
+++ lance.py
@@ -1,8 +1,10 @@
+from operator import itemgetter
 
-from locutils import haversine
 from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from geodist import haversine
+from gmaps_service import req_dist_matrix
 
 app = Flask(__name__)
 
@@ -17,19 +19,32 @@ class Vehicle(db.Model):
     plateno = db.Column(db.String())
     lat = db.Column(db.Float)
     longt = db.Column(db.Float())
+    isactive = db.Column(db.Boolean())
 
 
-@app.route('/dispatch/vehicle/destination=<lat>,<longt>', methods=['GET'])
+@app.route('/dispatch/destination=<lat>,<longt>', methods=['GET'])
 def get_vehicle(lat, longt):
-    locations = Vehicle.query.with_entities(
-        Vehicle.id, Vehicle.lat, Vehicle.longt).all()
-    results = []
-    for item in locations:
+    vehicle_locs = Vehicle.query.with_entities(
+        Vehicle.id, Vehicle.lat, Vehicle.longt).filter_by(isactive=True)
+    distances = []
+    for item in vehicle_locs:
         val = haversine(float(lat), float(longt), item[1], item[2])
-        results.append(val)
-    return jsonify(results)
+        distances.append({'id': item[0], 'distance': val})
+
+    srtd_by_dist = sorted(distances, key=itemgetter('distance'))
+    closest_five_dist = srtd_by_dist[:6]
+    print(closest_five_dist)
+    ids_for_closest_five = [x['id'] for x in closest_five_dist]
+    locs_for_req = [x for x in vehicle_locs if x[0] in ids_for_closest_five]
+    result = req_dist_matrix(lat, longt, locs_for_req)
+    print(result)
+    """id_shortest_dist = srtd_by_dist[0]['id']
+    veh = Vehicle.query.with_entities(
+        Vehicle.drivername, Vehicle.plateno).filter_by(id=id_shortest_dist).all()
+    vehdict = {'drivername': veh[0][0], 'plateno': veh[0][1]}"""
+
+    return jsonify(result)
 
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(debug=True)
